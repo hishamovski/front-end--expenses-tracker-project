ec4164d946091baad5380a2d608251450ac566c7
hishamovski <hisham.boualam@gmail.com>
2019-06-13 22:25:45 -0400

    Save Changes

--- assets/scripts/auth/events.js
+++ assets/scripts/auth/events.js
@@ -9,17 +9,10 @@ const onSignUp = event => {
 
   const form = event.target
   const formData = getFormFields(form)
-  if (isValid(formData.credentials.password, formData.credentials.password_confirmation)) {
-    api.signUp(formData)
-      .then(ui.onSignUpSuccess)
-      .catch(ui.onSignUpFailure)
-  } else {
-    $('.password-validation').show()
-    $('.password-validation').text('Password must contain at least 6 characters, at least one lower case and one Upper case')
-    setTimeout(function () {
-      $('.password-validation').hide()
-    }, 6000)
-  }
+
+  api.signUp(formData)
+    .then(ui.onSignUpSuccess)
+    .catch(ui.onSignUpFailure)
 }
 
 const onSignIn = event => {
@@ -48,30 +41,6 @@ const onSignOut = event => {
   api.signOut()
     .then(ui.onSignOutSuccess)
     .catch(ui.onSignOutFailure)
-}
-
-const isValid = (password, password2) => {
-  let isLengthValid = false
-  let isUpCase = false
-  let isLowCase = false
-
-  for (let i = 0; i < password.length; i++) {
-    if (password.length > 5 && password2.length > 5) {
-      isLengthValid = true
-    }
-    if (password[i] === password[i].toUpperCase() && password2[i] === password2[i].toUpperCase()) {
-      isUpCase = true
-    }
-    if (password[i] === password[i].toLowerCase() && password2[i] === password2[i].toLowerCase()) {
-      isLowCase = true
-    }
-  }
-
-  if (isLengthValid && isUpCase && isLowCase) {
-    return true
-  } else {
-    return false
-  }
 }
 
 module.exports = {
